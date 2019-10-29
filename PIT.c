943938e13b43953dcf0f0b5cbed537ffea1e31f4
Gonzalo Silva <30927470+gonzalosil@users.noreply.github.com>
2019-10-29 15:18:18 -0300

    Add files via upload
    creo que pit listo

--- PIT.c
+++ PIT.c
@@ -11,6 +11,17 @@
 
 #include "PIT.h"
 
+void PIT0_IRQHandler(void);
+void PIT1_IRQHandler(void);
+void PIT2_IRQHandler(void);
+void PIT3_IRQHandler(void);
+
+static void PIT_call_handler (uint8_t timer_num);
+
+static uint8_t pit_IRQ[] = {PIT0_IRQn, PIT1_IRQn, PIT2_IRQn, PIT3_IRQn};
+
+
+static PIT_funcallback funcs[4];
 
 void PIT_init (void){
 	SIM->SCGC6 |= SIM_SCGC6_PIT(1);
@@ -18,10 +29,17 @@ void PIT_init (void){
 }
 
 
-void PIT_set_timer (uint8_t frecuency, uint8_t timer_num){
+void PIT_IRQ (PIT_funcallback callback, uint8_t timer_num){
+	reset_flag(timer_num);
+	PIT->CHANNEL[timer_num].TCTRL |= PIT_TCTRL_TIE_MASK;
+	funcs[timer_num] = callback;
+	NVIC_EnableIRQ(pit_IRQ[timer_num]);
 
+}
+
+void PIT_set_timer (uint32_t frecuency, uint8_t timer_num){
 	PIT->CHANNEL[timer_num].LDVAL = (uint32_t)((CLOCK_FRECUENCY/frecuency) - 1);
-	PIT->CHANNEL[timer_num].TCTRL = PIT_TCTRL_TIE_MASK;
+	//PIT->CHANNEL[timer_num].TCTRL = PIT_TCTRL_TIE_MASK;
 	PIT->CHANNEL[timer_num].TCTRL |= PIT_TCTRL_TEN(1);
 }
 
@@ -33,3 +51,28 @@ uint32_t check_flag(uint8_t timer_num){
 void reset_flag(uint8_t timer_num){
 	PIT->CHANNEL[timer_num].TFLG &= PIT_TFLG_TIF_MASK;
 }
+
+void PIT_call_handler (uint8_t timer_num) {
+	funcs[timer_num]();
+	reset_flag(timer_num);
+	/*for (int i = 0; i < 32; i++){
+		if ((PORT_PCR_ISF_MASK & ports[port]->PCR[i])){
+			(*(funcs[port][i]))();
+			ports[port]->PCR[i] |= PORT_PCR_ISF_MASK;
+		}
+	}*/
+}
+
+void PIT0_IRQHandler(void){
+	PIT_call_handler(0);
+}
+void PIT1_IRQHandler(void){
+	PIT_call_handler(1);
+}
+void PIT2_IRQHandler(void){
+	PIT_call_handler(2);
+}
+void PIT3_IRQHandler(void){
+	PIT_call_handler(3);
+}
+
--- PIT.h
+++ PIT.h
@@ -12,9 +12,11 @@
 
 typedef PIT_Type *PIT_ptr;
 
+typedef void (*PIT_funcallback)(void);
 
 void PIT_init(void);
-void PIT_set_timer (uint8_t frecuency, uint8_t timer_num);
+void PIT_set_timer (uint32_t frecuency, uint8_t timer_num);
+void PIT_IRQ (PIT_funcallback callback, uint8_t timer_num);
 uint32_t check_flag(uint8_t timer_num);
 void reset_flag(uint8_t timer_num);
 
